6c439a0a0c19fab3dbd5984cc72f36408bdee2a9
= <=>
2024-05-28 15:44:15 +0100

    Correct the email

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -22,19 +22,19 @@ const Section2 = () => {
         <h2 className="text-[2.5rem] mb-4">Contact</h2>
         <div className="flex flex-row-reverse justify-around">
           <div className="flex items-center mb-2 hover:text-[#42bba8]">
-            <FaMapMarkerAlt size={24} className="mr-2" />
+            <FaMapMarkerAlt size={18} className="mr-2" />
             <a href="https://maps.app.goo.gl/fz2SZ19aguAHfc7K8" target="_blank" rel="noopener noreferrer">
               <span className='text-[1.2rem]'>Technopar, Ave Mohammed V, Tangier 90000</span>
             </a>
           </div>
           <div className="flex items-center mb-2 hover:text-[#42bba8]">
-            <FaEnvelope size={24} className="mr-2" />
-            <a href="mailto:contact@example.com">
+            <FaEnvelope size={18} className="mr-2" />
+            <a href="mailto:contact@dinomite.com">
               <span className='text-[1.2rem]'>contact@dinomite.com</span>
             </a>
           </div>
           <div className="flex items-center mb-2 hover:text-[#42bba8]">
-            <FaPhone size={24} className="mr-2" />
+            <FaPhone size={18} className="mr-2" />
             <a href="tel:+33123456789">
               <span className='text-[1.2rem]'>+212808526154</span>
             </a>
